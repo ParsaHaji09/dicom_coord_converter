37c17497f2e00b96d6789a304b3bd58894ec3720
Parsa Hajipour <47697224+ParsaHaji09@users.noreply.github.com>
2024-01-11 22:15:49 -0800

    test code

--- script_converter.py
+++ script_converter.py
@@ -20,6 +20,7 @@ def get_3D_coordinates(directory_path):
 
     #transformation matrix that maps 2D pixels to 3D coordinates
     transformation_matrix = imhelp.get_pixel_to_patient_transformation_matrix(series_data)
+    print("Transformation Matrix")
     print(transformation_matrix)
 
 
@@ -31,16 +32,26 @@ def get_3D_coordinates(directory_path):
     #potentially need to edit later
     dumbmask = np.zeros((data.shape[0], data.shape[1], 19), dtype=bool)
     dumbmask[100:250, 100:200, 2:-1] = 1
+    third_slice = dumbmask[:, :, 2]
+    
 
     data_roi = ROIData()
     data_roi.mask = dumbmask
+
+    contours, _ = imhelp.find_mask_contours(third_slice, data_roi.approximate_contours)
     result = imhelp.get_contours_coords(data_roi, series_data)
 
-
-    #check results
-    plt.imshow(dumbmask[:, :, 10])
-    plt.show()
+    result_array = np.array(contours)
+    fig = plt.figure()
+    ax = fig.add_subplot( projection='3d')
+    ax.scatter(result_array[:, 0], result_array[:, 1], result_array[:, 2])
+    
+    print("third slice")
+    print(contours)
+    print("Contours")
     print(result)
+
+    plt.show()
 
 
 inp = "./test_data/"
